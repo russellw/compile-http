c31004cd1e1cf334940160b57c1511479bcf7023
Russell Wallace <russell.wallace@gmail.com>
2023-10-18 01:47:40 +0100

    clean

--- compile-http.cc
+++ compile-http.cc
@@ -15,11 +15,8 @@
 #include <iostream>
 #include <iterator>
 #include <ostream>
-#include <set>
 #include <stdexcept>
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
 using namespace std;
 using filesystem::path;
@@ -30,52 +27,39 @@ using filesystem::path;
 #define debug(a) cout << __FILE__ << ':' << __LINE__ << ": " << __func__ << ": " << #a << ": " << (a) << '\n'
 #endif
 
-// input
-string file;
-string text;
-
-void readText() {
-	ifstream is(file, ios::in);
-	text = {istreambuf_iterator<char>(is), istreambuf_iterator<char>()};
-
-	// make sure input ends with a newline, to simplify parser code
-	if (text.empty() || text.back() != '\n')
-		text += '\n';
-}
-
-// SORT
 void decl(ostream& os, string name, size_t n) {
 	os << "const unsigned char " << name << "Data[" << to_string(n) << ']';
 }
 
-bool eq(const char* s, const char* t) {
-	for (auto i = strlen(t); i--;)
-		if (*s++ != *t++)
-			return 0;
-	return 1;
-}
-
-string esc(string s) {
-	string o = "\"";
-	for (auto c: s) {
-		if (isprint((unsigned char)c)) {
-			if (c == '"')
-				o += '\\';
-			o += c;
-			continue;
-		}
-		char buf[7];
-		sprintf_s(buf, sizeof buf, "\\x%02x\"\"", (unsigned char)c);
-		o += buf;
-	}
-	return o + '"';
-}
-
-//
 int main(int argc, char** argv) {
 	try {
+		vector<string> files;
 		for (int i = 1; i < argc; ++i) {
-			file = argv[i];
+			auto s = argv[i];
+			if (*s == '-') {
+				while (*s == '-')
+					++s;
+				switch (*s) {
+				case 'V':
+				case 'v':
+					cout << "compile-http version 0\n";
+					return 0;
+				case 'h':
+					cout << "Usage: compile-http [options] files\n";
+					cout << "Writes compiled-http.cxx,hxx\n";
+					cout << "\n";
+					cout << "-h  Show help\n";
+					cout << "-V  Show version\n";
+					return 0;
+				}
+				throw runtime_error(string(argv[i]) + ": unknown option");
+			}
+			files.push_back(s);
+		}
+
+		remove("compiled-http.cxx");
+		remove("compiled-http.hxx");
+		for (auto& file: files) {
 			auto name = path(file).stem().string();
 
 			// input file
